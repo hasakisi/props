482437c1afb3416875b7dd00f5085ae4ec7d6158
hasakisi <115834473+hasakisi@users.noreply.github.com>
2023-12-28 14:32:33 +0300

    Add files via upload

---
+++ components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text} from "react-native";
+
+const Header = () => {
+    return (
+
+        <View>
+            <Text>HEADER</Text>
+        </View>
+
+    );
+};
+
+export default Header;  
